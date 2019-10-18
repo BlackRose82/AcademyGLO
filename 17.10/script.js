d4a086f4c7bc46fb1c05971bb562dd055b8a21e3
Cristina Shapira <dinamic82@gmail.com>
2019-10-18 12:11:28 +0300

    Add files via upload

--- 17.10/script.js
+++ 17.10/script.js
@@ -1,8 +1,9 @@
-var money = "dollar";
-var income = "middle";
-var addExpenses = "maximum";
-var deposit = "minimal";
-var mission = "free";
-var period = "two month";
+let money = "dollar";
+let income = "middle";
+let addExpenses = "maximum";
+let deposit = "minimal";
+let mission = "JS";
+let period = "two month";
 
 console.log(period);
+alert(' Hello Academy');
