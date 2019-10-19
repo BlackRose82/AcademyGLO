3f62a9e0cac98d9d6afd6e0112e2fec7dc1a56d9
Cristina Shapira <dinamic82@gmail.com>
2019-10-19 23:59:05 +0300

    Update script.js

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,1 +1,9 @@
+let money = "dollar";
+let income = "middle";
+let addExpenses = "maximum";
+let deposit = "minimal";
+let mission = "JS";
+let period = "two month";
 
+console.log(period);
+alert(' Hello Academy');
