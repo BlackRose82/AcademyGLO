a8106f05f1e94f42c0f8ca92e3702f8358d36392
Cristina Shapira <dinamic82@gmail.com>
2019-10-19 19:03:17 +0300

    Add files via upload

---
+++ lesson02/script.js
@@ -0,0 +1,23 @@
+let money = 950;
+let income = 'social';
+let addExpenses = 'Hotel,courses,flights';
+let deposit = true;
+let mission = 35e3;
+let period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log('Период',+period+'','месяцев','и','Цель','заработать',+mission+'','долларов');
+// console.log('Период',+period+'','месяцев');
+// console.log('Цель','заработать',+mission+'','долларов');
+console.log(addExpenses.toLowerCase());
+for(let i = 0; i < 3; i++ ){
+    console.log(addExpenses);
+}
+const budgetDay = 950;
+console.log(1950/30);
+console.log(1950%30);
+
+
