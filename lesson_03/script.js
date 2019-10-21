2f10d03cdbfba42592ed19d3066759d74f05f581
Cristina Shapira <dinamic82@gmail.com>
2019-10-22 01:38:35 +0300

    Add files via upload

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -8,6 +8,7 @@ let income = 'social';
 let period = 6;
 let question2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', addExpenses, 'Во сколько это обойдется?', period);
 let mission = 35e3;
+let budgetDay = 310;
 
 console.log('Перечислите возможные расходы за рассчитываемый период', Array(addExpenses));
 console.log('Есть ли у вас депозит в банке?', deposit);
@@ -17,7 +18,13 @@ console.log(typeof income);
 // console.log(budgetMonth(Number('money') -Number('550'));
 console.log(Math.max(mission));
 // console.log(Math.min(budgetMonth));
-
+if(budgetDay>800){
+    console.log("Высокий уровень дохода");
+}else if(800>budgetDay>300){
+    console.log("Средний доход");
+}else(300>budgetDay>0){
+    console.log("Низкий доход");
+}
 
 
 
